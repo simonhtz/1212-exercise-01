ef240feea6c40b145201c7c88ede5b17ebee6723
simon_no <shiamson@hotmail.com>
2023-04-12 14:33:21 +0700

    Finished the exercise

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const randomFolks = document.querySelector(".random-peeps");
-const getData = async function () {
-    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+const selectUserNumber = document.querySelector("#users");
+
+const getData = async function (numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const userResults = data.results;
     displayUsers(userResults);
 };
-getData();
+getData(1);
 
 const displayUsers = function(userResults) {
     randomFolks.innerHTML = "";
@@ -20,3 +22,7 @@ const displayUsers = function(userResults) {
         randomFolks.append(userDiv);
     }
 };
+selectUserNumber.addEventListener("change", function(e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+})
